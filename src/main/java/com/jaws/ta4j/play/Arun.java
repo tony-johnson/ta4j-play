ede45ff4969cf5b9bc3ffac3ad65f64fc1848ddc
arujohn <arujohn@10.0.0.145>
2017-09-22 07:21:05 -0700

    Added three loops to determine profit for all variable combination

--- src/main/java/com/jaws/ta4j/play/Arun.java
+++ src/main/java/com/jaws/ta4j/play/Arun.java
@@ -37,18 +37,28 @@ import ta4jexamples.loaders.QuandlTicksFileLoader;
 public class Arun {
 
     public static void main(String[] args) {
+        for (int b = 1; b < 20; b++) {
+            for (int c = 1; c < 50; c++) {
+                for (int d = 1; d < 100; d++) {
+                    doOneTest(b, c, d);
+                }
+            }
+        }
+    }
+
+    private static void doOneTest(int b, int c, int d) {
         List<Double> results = Collections.synchronizedList(new ArrayList<>());
         String[] SP500_symbol = new String[]{"AAPL", "ABT", "ABBV", "ACN", "ACE", "ADBE", "ADT", "AAP", "AES", "AET", "AFL","AMG", "A", "GAS", "ARE", "APD", "AKAM", "AA", "AGN", "ALXN", "ALLE", "ADS", "ALL", "ALTR", "MO", "AMZN", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AMP", "ABC", "AME", "AMGN", "APH", "APC", "ADI", "AON", "APA", "AIV", "AMAT", "ADM", "AIZ", "T", "ADSK", "ADP", "AN", "AZO", "AVGO", "AVB", "AVY", "BHI", "BLL", "BAC", "BK", "BCR", "BXLT", "BAX", "BBT", "BDX", "BBBY", "BRK.B", "BBY", "BLX", "HRB", "BA", "BWA", "BXP", "BSX", "BMY", "BRCM", "BF.B", "CHRW", "CA", "CVC", "COG", "CAM", "CPB", "COF", "CAH", "HSIC", "KMX", "CCL", "CAT", "CBG", "CBS", "CELG", "CNP", "CTL", "CERN", "CF", "SCHW", "CHK", "CVX", "CMG", "CB", "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CTXS", "CLX", "CME", "CMS", "COH", "KO", "CCE", "CTSH", "CL", "CMCSA", "CMA", "CSC", "CAG", "COP", "CNX", "ED", "STZ", "GLW", "COST", "CCI", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DLPH", "DAL", "XRAY", "DVN", "DO", "DTV", "DFS", "DISCA", "DISCK", "DG", "DLTR", "D", "DOV", "DOW", "DPS", "DTE", "DD", "DUK", "DNB", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMC", "EMR", "ENDP", "ESV", "ETR", "EOG", "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ES", "EXC", "EXPE", "EXPD", "ESRX", "XOM", "FFIV", "FB", "FAST", "FDX", "FIS", "FITB", "FSLR", "FE", "FISV", "FLIR", "FLS", "FLR", "FMC", "FTI", "F", "FOSL", "BEN", "FCX", "FTR", "GME", "GPS", "GRMN", "GD", "GE", "GGP", "GIS", "GM", "GPC", "GNW", "GILD", "GS", "GT", "GOOGL", "GOOG", "GWW", "HAL", "HBI", "HOG", "HAR", "HRS", "HIG", "HAS", "HCA", "HCP", "HCN", "HP", "HES", "HPQ", "HD", "HON", "HRL", "HSP", "HST", "HCBK", "HUM", "HBAN", "ITW", "IR", "INTC", "ICE", "IBM", "IP", "IPG", "IFF", "INTU", "ISRG", "IVZ", "IRM", "JEC", "JBHT", "JNJ", "JCI", "JOY", "JPM", "JNPR", "KSU", "K", "KEY", "GMCR", "KMB", "KIM", "KMI", "KLAC", "KSS", "KRFT", "KR", "LB", "LLL", "LH", "LRCX", "LM", "LEG", "LEN", "LVLT", "LUK", "LLY", "LNC", "LLTC", "LMT", "L", "LOW", "LYB", "MTB", "MAC", "M", "MNK", "MRO", "MPC", "MAR", "MMC", "MLM", "MAS", "MA", "MAT", "MKC", "MCD", "MCK", "MJN", "MMV", "MDT", "MRK", "MET", "KORS", "MCHP", "MU", "MSFT", "MHK", "TAP", "MDLZ", "MON", "MNST", "MCO", "MS", "MOS", "MSI", "MUR", "MYL", "NDAQ", "NOV", "NAVI", "NTAP", "NFLX", "NWL", "NFX", "NEM", "NWSA", "NEE", "NLSN", "NKE", "NI", "NE", "NBL", "JWN", "NSC", "NTRS", "NOC", "NRG", "NUE", "NVDA", "ORLY", "OXY", "OMC", "OKE", "ORCL", "OI", "PCAR", "PLL", "PH", "PDCO", "PAYX", "PNR", "PBCT", "POM", "PEP", "PKI", "PRGO", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PBI", "PCL", "PNC", "RL", "PPG", "PPL", "PX", "PCP", "PCLN", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PSA", "PHM", "PVH", "QRVO", "PWR", "QCOM", "DGX", "RRC", "RTN", "O", "RHT", "REGN", "RF", "RSG", "RAI", "RHI", "ROK", "COL", "ROP", "ROST", "RLD", "R", "CRM", "SNDK", "SCG", "SLB", "SNI", "STX", "SEE", "SRE", "SHW", "SPG", "SWKS", "SLG", "SJM", "SNA", "SO", "LUV", "SWN", "SE", "STJ", "SWK", "SPLS", "SBUX", "HOT", "STT", "SRCL", "SYK", "STI", "SYMC", "SYY", "TROW", "TGT", "TEL", "TE", "TGNA", "THC", "TDC", "TSO", "TXN", "TXT", "HSY", "TRV", "TMO", "TIF", "TWX", "TWC", "TJX", "TMK", "TSS", "TSCO", "RIG", "TRIP", "FOXA", "TSN", "TYC", "UA", "UNP", "UNH", "UPS", "URI", "UTX", "UHS", "UNM", "URBN", "VFC", "VLO", "VAR", "VTR", "VRSN", "VZ", "VRTX", "VIAB", "V", "VNO", "VMC", "WMT", "WBA", "DIS", "WM", "WAT", "ANTM", "WFC", "WDC", "WU", "WY", "WHR", "WFM", "WMB", "WEC", "WYN", "WYNN", "XEL", "XRX", "XLNX", "XL", "XYL", "YHOO", "YUM", "ZBH", "ZION", "ZTS"};
         List<String> symbols = Arrays.asList(SP500_symbol);
         symbols.parallelStream().filter((ticker) -> !(ticker.contains("."))).filter((ticker) -> !(ticker.equals("MMV"))).forEach((ticker) -> {
-            double result = test(ticker);
+            double result = test(ticker,d,c,b);
             results.add(result);
-            System.out.printf("Stock %s result %5.5g\n",ticker,result);
+            //System.out.printf("Stock %s result %5.5g\n",ticker,result);
         });
         OptionalDouble average = results.stream().mapToDouble(a->a).average();
-        System.out.printf("Mean Percent Profit is: %5.5g\n",average.getAsDouble());
+        System.out.printf("%5.5g, %d, %d, %d\n",average.getAsDouble(),d,c,b);
     }
-    public static double test(String ticker) {
+    public static double test(String ticker, int longPeriod, int shortPeriod, int signalPeriod) {
         
         
         QuandlTicksFileLoader.QuandlDataFeed quandl = new QuandlTicksFileLoader.QuandlDataFeed("WIKI-"+ticker, DateTime.parse("2015-09-01"), DateTime.parse("2017-09-01"));
@@ -57,11 +67,11 @@ public class Arun {
         // Or within an indicator:
         ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
         // Here is the same close price:
-        LinearRegressionSlopeIndicator slr12 = new LinearRegressionSlopeIndicator(closePrice,12);
-        LinearRegressionSlopeIndicator slr26 = new LinearRegressionSlopeIndicator(closePrice,26);
+        LinearRegressionSlopeIndicator slr12 = new LinearRegressionSlopeIndicator(closePrice,shortPeriod);
+        LinearRegressionSlopeIndicator slr26 = new LinearRegressionSlopeIndicator(closePrice,longPeriod);
 
         DifferenceIndicator diff = new DifferenceIndicator(slr12,slr26);        
-        SMAIndicator ema = new SMAIndicator(diff,8);
+        SMAIndicator ema = new SMAIndicator(diff,signalPeriod);
         
         Rule buyingRule = new CrossedUpIndicatorRule(ema, Decimal.valueOf("0"));
         Rule sellingRule = new CrossedDownIndicatorRule(ema, Decimal.valueOf("0"));
@@ -121,6 +131,7 @@ public class Arun {
 //
 //        BuyAndSellSignalsToChart.displayChart(chart);
     }
+    
 
     private static void addTrades(TimeSeries series, TradingRecord tr, XYPlot plot) {
         // Adding markers to plot
